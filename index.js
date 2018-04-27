0038bfb354245e64a38867286a33feb24a916241
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-04-27 16:52:54 -0400

    adds initial support for valid networks and unlocked accounts

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ export default class web3Manager extends EventEmitter {
   constructor(interval, requiredNetwork, localProvider){
     super();
     this.web3 = null;
-    // this.account = null;
-    // this.validNetwork = null;
-    // this.requiredNetwork = requiredNetwork || null;
-    // this.interval = interval || 500;
-    // this.localProvider = localProvider || null;
+    this.account = null;
+    this.validNetwork = false;
+    this.requiredNetwork = requiredNetwork || null;
+    this.interval = interval || 500;
+    this.localProvider = localProvider || null;
+    this.intervalID = null;
   }
   start(){
-    setInterval(this.fetchWeb3Data.bind(this), this.interval);
+    this.intervalID = setInterval(this.fetchWeb3Data.bind(this), this.interval);
   }
-  stop(){}
+  stop(){
+    clearInterval(this.intervalID);
+  }
   hasWeb3(callback){
     // registers the listener for hasWeb3 and provides callback to tell class what to do when hasWeb3 event fires
-    this.on('hasWeb3', () => callback(web3));
+    this.on('hasWeb3', (web3) => callback(web3));
+  }
+  loggedInToNewAccount(callback){
+    this.on('loggedInToNewAccount', (account) => callback(account));
+  }
+  onValidNetwork(callback){
+    this.on('onValidNetwork', (onValidNetwork) => callback(this.onValidNetwork));
   }
   async fetchWeb3Data (){
     const web3 = this.getWeb3FromWindow();
@@ -31,26 +40,30 @@ export default class web3Manager extends EventEmitter {
       }
 
       /* ---------- ensures the user is on the right network ----------- */
-      // const currentNetworkId = Number(web3.version.network);
+      const currentNetworkId = Number(web3.version.network);
 
-      // // if component received a validNetwork prop, we make sure the user is on the valid network
-      // const onCorrectNetwork = this.requiredNetwork ?
-      // this.requiredNetwork === currentNetworkId : true;
+      // if component received a validNetwork prop, we make sure the user is on the valid network
+      const onCorrectNetwork = this.requiredNetwork ?
+      this.requiredNetwork === currentNetworkId : true;
 
-      // // valid network refers to the previous bool value kept on redux store
-      // const changedToValidNetwork = onCorrectNetwork && !this.validNetwork;
+      // valid network refers to the previous bool value kept on redux store
+      const changedToValidNetwork = onCorrectNetwork && !this.validNetwork;
 
-      // if (changedToValidNetwork) // EVENT
+      if (changedToValidNetwork) {
+        this.validNetwork = true;
+        this.emit('onValidNetwork', true);
+      }
 
-      // /* ------------- checks for unlocked account change -------------- */
-      // const [ account ] = await web3.eth.getAccounts();
-      // const recentlyChangedAccount = account && account !== currentAccount;
-      // const recentlyLoggedOut = !account && currentAccount;
+      /* ------------- checks for unlocked account change -------------- */
+      const [ account ] = await web3.eth.getAccounts();
+      const recentlyChangedAccount = account && account !== this.account;
+      const recentlyLoggedOut = !account && this.account;
 
-      // // if an important account changed, dispatch the appropriate action
-      // if (recentlyChangedAccount || recentlyLoggedOut) {
-      //   //EVENT
-      // }
+      // if an important account changed, dispatch the appropriate action
+      if (recentlyChangedAccount || recentlyLoggedOut) {
+        this.account = account;
+        this.emit('loggedInToNewAccount', account);
+      }
     }
   }
   getWeb3FromWindow(){
